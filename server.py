22a2a77155a7b521860a4021a3f94fbd0fca4164
madman61rus <rhjnhjtn3yjhe>
2017-03-07 00:46:00 +0300

    + Добавил git submodule 13_cinemas

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
+import sys
+sys.path.append("./13_cinemas")
+from cinemas import fetch_afisha_page,parse_afisha_list,fetch_movie_info
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def films_list():
-    return render_template('films_list.html')
+    afisha_page = fetch_afisha_page('http://www.afisha.ru/msk/schedule_cinema/')
+    afisha_list = parse_afisha_list(afisha_page)
+
+    return render_template('films_list.html',afisha_list=afisha_list)
+
 
 if __name__ == "__main__":
     app.run()
